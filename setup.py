5b1ef0b67e2e3babae0e39ceeb5db12353121dae
misa_misa <105995479+mandrejezieski@users.noreply.github.com>
2023-04-13 05:07:03 -0300

    up

---
+++ setup.py
@@ -0,0 +1,27 @@
+import sys
+from cx_Freeze import setup, Executable
+import pygame
+
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [
+        Executable("script.py", base=base)
+]
+
+buildOptions = dict(
+        packages = [],
+        includes = [pygame, sys, locals(),random],
+        include_files = [],
+        excludes = []
+)
+
+setup(
+    name = "Test_cobrinha_002",
+    version = "1.0",
+    description = ": GAME DA COBRINHA : ",
+    options = dict(build_exe = buildOptions),
+    executables = executables
+ )
