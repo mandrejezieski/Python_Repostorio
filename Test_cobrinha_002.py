f449653c8dc57a48e3514232aa4b1ed5fbd675d0
misa_misa <105995479+mandrejezieski@users.noreply.github.com>
2023-04-13 02:45:36 -0300

    update

--- Test_cobrinha_002.py
+++ Test_cobrinha_002.py
@@ -17,10 +17,11 @@ y_cobra = int(altura / 2)
 velocidade = 10
 x_controle = 15
 y_controle = 0
+vel = 10
 
 ### SEGUNDO ELEMENTO DO JOGO ("O ALIMENTO"):
-x_maça = randint(40, 1240)
-y_maça = randint(50, 930)
+x_maca = randint(40, 580)
+y_maca = randint(50, 380)
 
 ### CONFIGURAÇÃO DO CONTADOR DE PONTOS:
 pontos = 0
@@ -33,36 +34,36 @@ relogio = pygame.time.Clock()
 
 ### TAMANHO INICIAL DA COBRA
 lista_cobra = []
-comprimento_inicial = 1
+comprimento_inicial = 3
 
 morreu = False
 
 
 def aumenta_cobra(lista_cobra):
     for XeY in lista_cobra:
-        pygame.draw.rect(tela, (102, 102, 0), (XeY[0], XeY[1], 20, 20))
+        pygame.draw.rect(tela, (50, 100, 150), (XeY[0], XeY[1], 20, 20))
 
 
 ### REINICIO DO JOGO APÓS O GAME OVER
 def reiniciar_jogo():
-    global pontos, comprimento_inicial, x_cobra, y_cobra, lista_cobra, lista_cabeça, x_maça, y_maça, morreu
+    global pontos, comprimento_inicial, x_cobra, y_cobra, lista_cobra, lista_cabeça, x_maca, y_maca, morreu
     pontos = 0
     comprimento_inicial = 1
     x_cobra = int(largura / 2)
     y_cobra = int(altura / 2)
     lista_cobra = []
     lista_cabeça = []
-    x_maça = randint(40, 550)
-    y_maça = randint(50, 350)
+    x_maça = randint(40, 580)
+    y_maça = randint(50, 380)
     morreu = False
 
 
 ### REPETIÇÃO DO JOGO (TODOS OS JOGOS SE PASSAM NUM LOOP INFINITO):
 while True:
-    relogio.tick(10)
-    tela.fill((255, 255, 255))
+    relogio.tick(vel)
+    tela.fill((50, 50, 50))
     mensagem = f'Pontos: {pontos}'
-    texto_formatado = fonte.render(mensagem, False, (0, 0, 0))
+    texto_formatado = fonte.render(mensagem, True, (0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -100,15 +101,16 @@ while True:
     y_cobra = y_cobra + y_controle
 
     ### PERSONAGENS
-    cobra = pygame.draw.rect(tela, (102, 102, 0), (x_cobra, y_cobra, 20, 20))
-    maça = pygame.draw.rect(tela, (255, 51, 51), (x_maça, y_maça, 20, 20))
+    cobra = pygame.draw.rect(tela, (50, 100, 150), (x_cobra, y_cobra, 20, 20))
+    maca = pygame.draw.rect(tela, (255, 0, 0), (x_maca, y_maca, 20, 20))
 
     ### CONDIÇÃO DE COLISÃO:
-    if cobra.colliderect(maça):
-        x_maça = randint(40, )
-        y_maça = randint(50, 930)
+    if cobra.colliderect(maca):
+        x_maca = randint(40, 580)
+        y_maca = randint(50, 380)
         pontos += 1
-        som_da_colisao.play()
+        vel += 1
+        #som_da_colisao.play()
         comprimento_inicial = comprimento_inicial + 1
 
     ### CRIA O CORPO DA COBRA
@@ -126,7 +128,7 @@ while True:
 
         morreu = True
         while morreu:
-            tela.fill((255, 255, 255))
+            tela.fill((20, 80, 140))
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
@@ -155,5 +157,5 @@ while True:
 
     aumenta_cobra(lista_cobra)
 
-    tela.blit(texto_formatado, (1070, 30))
+    tela.blit(texto_formatado, (400, 20))
     pygame.display.update()
